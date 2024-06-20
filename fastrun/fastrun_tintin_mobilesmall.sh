bf144f73df2bd63a130cf71d052328d0e0eacda3
Anfeng Peng <anp407@tintin.cs.pitt.edu>
2024-06-20 18:09:20 -0400

    tintin

--- fastrun/fastrun_tintin_mobilesmall.sh
+++ fastrun/fastrun_tintin_mobilesmall.sh
@@ -1,3 +1,3 @@
 #! bin/sh
 
-python train.py --dataset imagenet --device tintin --model mobilenetV3 --mobilev3size large --cuda 3 --batch 128 
+python train.py --dataset imagenet --device tintin --model mobilenetV3 --mobilev3size small --cuda 3 --batch 128 

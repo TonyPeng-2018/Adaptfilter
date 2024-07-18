ffcb19479eb4a8184bc359bf8923c6c272ecf49d
TonyPeng-rpi <uoiap2021@gmail.com>
2024-07-18 15:27:41 -0400

    rpi

---
+++ rpi_simple_test_gate_speed.py
@@ -0,0 +1,111 @@
+# this file is for device on the client side
+
+# load the dataset
+
+# This file is for trainning
+# Run this on the server, or as we called offline. 
+
+import argparse
+import base64
+import cv2
+import datetime
+from Models import gatedmodel,mobilenetv2, resnet
+import numpy as np
+import os
+import PIL
+import psutil
+import sys
+import time
+import torch
+from tqdm import tqdm
+from Utils import utils, encoder
+
+middle_sizes_mobile = [1,2,4,8,16]
+middle_sizes_resnet = [1,2,4,8,16,32]
+
+middle_sizes = {'mobilenet': middle_sizes_mobile, 'resnet': middle_sizes_resnet}
+reduced_sizes = {'cifar-10': (32,32), 'imagenet': (224,224)}
+reduced_rates = {'mobilenet': 2, 'resnet': 4}
+
+dataset = 'cifar-10'
+model = 'resnet'
+i_stop = 100
+
+width, height = reduced_sizes[dataset][0]/reduced_rates[model], \
+                reduced_sizes[dataset][1]/reduced_rates[model]
+middle_size = middle_sizes[model]
+
+# client include client, middle and gate
+
+# client = mobilenetv2.mobilenetv2_splitter_client(
+#     num_classes = 10, 
+#     weight_root='./Weights/'+dataset+'/', 
+#     device='cpu')
+client = resnet.resnet_splitter_client(num_classes=1000, weight_root='./Weights/'+dataset+'/', device='cpu', layers=50)
+
+middle_models = []
+for i in range(len(middle_size)):
+    # middle_models.append(mobilenetv2.MobileNetV2_middle(middle=middle_size[i]))
+    middle_models.append(resnet.resnet_middle(middle=middle_size[i]))
+
+gate_models = []
+for i in range(len(middle_size)):
+    gate_models.append(gatedmodel.ExitGate(in_planes=middle_size[i],
+                                           height = height, width=width))
+
+# eval
+client.eval()
+for i in range(len(middle_size)):
+    middle_models[i].eval()
+    gate_models[i].eval()
+
+# quantize
+client = torch.ao.quantization.quantize_dynamic(client, {torch.nn.Linear, torch.nn.Conv2d}, dtype=torch.qint8)
+for i in range(len(middle_size)):
+    middle_models[i] = torch.ao.quantization.quantize_dynamic(middle_models[i], {torch.nn.Linear, torch.nn.Conv2d}, dtype=torch.qint8)
+    gate_models[i] = torch.ao.quantization.quantize_dynamic(gate_models[i], {torch.nn.Linear, torch.nn.Conv2d}, dtype=torch.qint8)
+
+# 2. dataset
+# directly read bmp image from the storage
+data_root = '../data/'+dataset+'-client/'
+images_list = os.listdir(data_root)
+images_list.remove('labels.txt')
+# remove ending with jpg
+images_list = [x for x in images_list if x.endswith('.bmp')]
+images_list = sorted(images_list)
+
+client_time = [0] * len(middle_size)
+
+# this is test the overspeed, so we don't need to load the models
+with torch.no_grad():
+        for i, i_path in tqdm(enumerate(images_list)):
+            if i >= i_stop:
+                break
+            
+            image_path = data_root + i_path
+            image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+            image = image.astype(np.float32)/255.0
+            image = torch.tensor(image)
+            image = image.unsqueeze(0)
+            image = image.permute(0, 3, 1, 2)
+            
+            with torch.no_grad():
+                s_time = time.time()
+                client_out = client(image).detach()
+                for j in range(len(middle_size)):
+                    middle_in = middle_models[j].in_layer(client_out)
+                    gate_out = gate_models[j](middle_in)
+                    middle_int = utils.float_to_uint(middle_in)
+                    middle_int = middle_int.numpy().copy(order='C')
+                    middle_int = middle_int.astype(np.uint8)
+                    send_in = base64.b64encode(middle_int)
+                    s1_time = time.time()
+                    client_time[j] += s1_time - s_time
+
+for i in range(len(client_time)):
+    client_time[i] /= i_stop / 1000 # ms per frame
+
+# print the list without [ and ]
+out_string = str(client_time).replace('[','').replace(']','')
+print(dataset, model)
+print(out_string)
---
+++ rpi_test_batch_client_JPEG_ML_speed.py
@@ -0,0 +1,147 @@
+# this file is for device on the client side
+
+# load the dataset
+
+# This file is for trainning
+# Run this on the server, or as we called offline. 
+
+import argparse
+import base64
+import cv2
+import datetime
+from Models import gatedmodel,mobilenetv2, resnet
+import numpy as np
+import os
+import PIL
+import psutil
+import sys
+import time
+import torch
+from tqdm import tqdm
+from Utils import utils, encoder
+
+gate_confidence = 0.85
+batch_size = 60
+
+dataset = 'cifar-10'
+i_stop = 10
+
+quality = '25'
+
+# 2. dataset
+# directly read bmp image from the storage
+data_root = '../data/'+dataset+'-client/'
+images_list = os.listdir(data_root)
+images_list.remove('labels.txt')
+# remove ending with jpg
+images_list = [x for x in images_list if x.endswith('.bmp')]
+images_list = sorted(images_list)
+
+client_time = [0] * batch_size * i_stop
+
+jpeg25_folder = '../data/'+dataset+'-jpeg25-ML/'
+jpeg75_folder = '../data/'+dataset+'-jpeg75-ML/'
+cjpeg_folder = '../data/'+dataset+'-cjpeg-ML/'
+
+if not os.path.exists(jpeg25_folder):
+    os.makedirs(jpeg25_folder)
+if not os.path.exists(jpeg75_folder):
+    os.makedirs(jpeg75_folder)
+if not os.path.exists(cjpeg_folder):
+    os.makedirs(cjpeg_folder)
+
+f = open('rpi-jpeg-ML.txt', 'w')
+
+from PIL import Image
+from DeepN import main_no_store
+import subprocess
+import os
+
+JPEG_time_25 = [0] * batch_size * i_stop
+JPEG_time_75 = [0] * batch_size * i_stop
+CJPEG_time = [0] * batch_size * i_stop  
+for i, i_path in tqdm(enumerate(images_list)):
+    image_path = data_root + i_path
+    image = Image.open(image_path).convert('RGB')
+    image = image.resize((32, 32))
+    image = np.array(image)
+    # 3. compress the image
+    # 3.1 JPEG 25
+    s_time = time.time()
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 25]
+    qtable = main_no_store(image)
+    result, encimg = cv2.imencode('.jpg', image, encode_param)
+    np.savetxt('qt.txt', qtable, fmt='%d')
+    command = 'cjpeg -dct int -qtable qt.txt -baseline'
+    command += ' -quality ' + str(25)
+    command += ' -outfile '
+    command += jpeg25_folder+ i_path[:-4]+'.jpg'
+    command += ' ' + str(image_path)
+    os.system(command)
+    new_image = Image.open(jpeg25_folder+ i_path[:-4]+'.jpg')
+    # encode
+    encimg = new_image.tobytes()
+    encimg = base64.b64encode(encimg)
+    e_time = time.time()
+    with open(jpeg25_folder + i_path[:-4], 'wb') as f2:
+        f2.write(encimg)
+            
+    JPEG_time_25[i] = e_time - s_time
+
+    # 3.3 JPEG 75
+    s_time = time.time()
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 25]
+    qtable = main_no_store(image)
+    result, encimg = cv2.imencode('.jpg', image, encode_param)
+    np.savetxt('qt.txt', qtable, fmt='%d')
+    command = 'cjpeg -dct int -qtable qt.txt -baseline'
+    command += ' -quality ' + str(75)
+    command += ' -outfile '
+    command += jpeg75_folder+ i_path[:-4]+'.jpg'
+    command += ' ' + str(image_path)
+    os.system(command)
+    new_image = Image.open(jpeg75_folder+ i_path[:-4]+'.jpg')
+    # encode
+    encimg = new_image.tobytes()
+    encimg = base64.b64encode(encimg)
+    e_time = time.time()
+    with open(jpeg75_folder + i_path[:-4], 'wb') as f2:
+        f2.write(encimg)
+    
+    JPEG_time_75[i] = e_time - s_time
+
+    # 3.4 progressive JPEG
+    s_time = time.time()
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 25]
+    qtable = main_no_store(image)
+    result, encimg = cv2.imencode('.jpg', image, encode_param)
+    np.savetxt('qt.txt', qtable, fmt='%d')
+    command = 'cjpeg -dct int -qtable qt.txt -baseline'
+    command += ' -progressive'
+    command += ' -outfile '
+    command += cjpeg_folder+ i_path[:-4]+'.jpg'
+    command += ' ' + str(image_path)
+    os.system(command)
+    new_image = Image.open(cjpeg_folder+ i_path[:-4]+'.jpg')
+    # encode
+    encimg = new_image.tobytes()
+    encimg = base64.b64encode(encimg)
+    e_time = time.time()
+    with open(cjpeg_folder + i_path[:-4], 'wb') as f2:
+        f2.write(encimg)
+    
+    CJPEG_time[i] = e_time - s_time
+
+    f.write(i_path + ' ' + str(JPEG_time_25[i]) + ' ' + str(JPEG_time_75[i]) + ' ' + str(CJPEG_time[i]) + '\n')
+avg_jpeg25 = [0] * i_stop
+avg_jpeg75 = [0] * i_stop
+avg_cjpeg = [0] * i_stop
+
+for i in range(i_stop):
+    avg_jpeg25[i] = sum(JPEG_time_25[i*batch_size:(i+1)*batch_size])/batch_size*1000
+    avg_jpeg75[i] = sum(JPEG_time_75[i*batch_size:(i+1)*batch_size])/batch_size*1000
+    avg_cjpeg[i] = sum(CJPEG_time[i*batch_size:(i+1)*batch_size])/batch_size*1000
+# print average time
+print('avg_jpeg25:', avg_jpeg25)
+print('avg_jpeg75:', avg_jpeg25)
+print('avg_cjpeg:', avg_cjpeg)

c8a4deb0466b935d5163c4de4edb89acc28d1684
tony peng <tonyy_good@126.com>
2024-07-23 01:32:12 -0400

    home

--- util_home_final_receiver.py
+++ util_home_final_receiver.py
@@ -11,7 +11,7 @@ class Server:
         # host = 'localhost'
         # host = '100.64.0.2'
         # host = '100.64.0.4'
-        host = '100.64.0.1'
+        host = '192.168.1.164'
         port = 5566
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -27,16 +27,18 @@ class Server:
             #                    'cifar-10-jpeg25/', 'cifar-10-jpeg75/', 'cifar-10-cjpeg/',
             #                     'cifar-10-jpeg25-ML/', 'cifar-10-jpeg75-ML/', 'cifar-10-cjpeg-ML/',
             #                     'imagenet-resnet-gate-emb/', 'cifar-10-mobile-gate-emb/']
-            dataset_subroot = [
-                               'cifar-10-jpeg25/', 'cifar-10-jpeg75/', 'cifar-10-cjpeg/',
-                                'cifar-10-jpeg25-ML/', 'cifar-10-jpeg75-ML/', 'cifar-10-cjpeg-ML/',
-                                'cifar-10-mobile-gate-emb/']
+            # dataset_subroot = [
+            #                    'cifar-10-jpeg25/', 'cifar-10-jpeg75/', 'cifar-10-cjpeg/',
+            #                     'cifar-10-jpeg25-ML/', 'cifar-10-jpeg75-ML/', 'cifar-10-cjpeg-ML/',
+            #                     'cifar-10-mobile-gate-emb/']
+            dataset_subroot = ['last-imagenet-20-jpeg'+str(x) for x in range(10, 100, 10)]
+
             client_socket, addr = self.s.accept()
             for ds in dataset_subroot:
-                packet_receive_time = [0] * 600
-                received_bytes = []
+                packet_receive_time = 0
+                received_bytes = 0
                 print("Got a connection from %s" % str(addr))
-                for i in tqdm(range(600)):
+                for i in tqdm(range(100)):
                     data = client_socket.recv(8) # length of encoded img
                     t1 = time.time()
                     length = unpack('>Q', data)[0]
@@ -44,23 +46,18 @@ class Server:
                     while len(data) < length:
                         data += client_socket.recv(1024 if length - len(data) > 1024 else length - len(data))
                     t2 = time.time()
-                    received_bytes.append(len(data))
+                    received_bytes += len(data)
                     # send received to the client
                     client_socket.sendall(b'1')
-                    packet_receive_time[i] = t2 - t1
-                # print the average time for each box
-                packet_box = [0]*10
-                print(np.mean(np.array(received_bytes)))
-                for i in range(10):
-                    packet_box[i] = np.mean(packet_receive_time[i*60:(i+1)*60])*1000
-                    # set four decimal places
-                    packet_box[i] = round(packet_box[i], 4)
-                print(ds, packet_box)
-                # remove [ ] in packet box when write
-                pb = str(packet_box)
-                pb = pb.replace('[', '').replace(']', '').replace(' ', '')
-                
-                f.write(ds[:-1] + ',' + str(packet_box) + '\n')
+                    packet_receive_time += t2 - t1
+                #
+                packet_receive_time = packet_receive_time * 1000
+                packet_receive_time = packet_receive_time / 600
+                received_bytes = received_bytes / 600
+                received_bytes = np.round(received_bytes, 2)
+                packet_receive_time = np.round(packet_receive_time, 2)
+                print('Average received bytes:', received_bytes)
+                print('Average packet receive time:', packet_receive_time)
         except Exception as e:
             print(e)
             self.s.close()
--- util_home_final_sender.py
+++ util_home_final_sender.py
@@ -8,13 +8,14 @@ from struct import pack
 import sys
 import time
 from tqdm import tqdm
+from PIL import Image
 
 class Sender:
     def __init__(self):
         # host = 'localhost'
         # host = '100.64.0.2'
         # host = '100.64.0.4'
-        host = '100.64.0.1'
+        host = '192.168.1.164'
         port = 5566
         self.host = host
         self.port = port
@@ -29,36 +30,71 @@ class Sender:
             #                    'cifar-10-jpeg25/', 'cifar-10-jpeg75/', 'cifar-10-cjpeg/',
             #                     'cifar-10-jpeg25-ML/', 'cifar-10-jpeg75-ML/', 'cifar-10-cjpeg-ML/',
             #                     'imagenet-resnet-gate-emb/', 'cifar-10-mobile-gate-emb/']
-            dataset_subroot = ['cifar-10-mobile-gate-emb/']
-            dataset_subroot = [
-                               'cifar-10-jpeg25/', 'cifar-10-jpeg75/', 'cifar-10-cjpeg/',
-                                'cifar-10-jpeg25-ML/', 'cifar-10-jpeg75-ML/', 'cifar-10-cjpeg-ML/',
-                                'cifar-10-mobile-gate-emb/']
-            for dataset in dataset_subroot:
-                dataset = dataset_root + dataset
-                print(dataset)
-
-                file_names = [str(i) for i in range(600)]
-                files = [open(dataset + '/' + file_name, 'rb') for file_name in file_names]
-                files = [file.read() for file in files]
-                
-                for i, file in enumerate(files):
-                    # print(len(file))
-                    # file = open(dataset + '/' + str(i), 'rb')
-                    # file = file.read()
-                    msg_length = pack('>Q', len(file))
-                    time.sleep(0.01)
+            # dataset_subroot = ['cifar-10-mobile-gate-emb/']
+            # dataset_subroot = [
+            #                    'cifar-10-jpeg25/', 'cifar-10-jpeg75/', 'cifar-10-cjpeg/',
+            #                     'cifar-10-jpeg25-ML/', 'cifar-10-jpeg75-ML/', 'cifar-10-cjpeg-ML/',
+            #                     'cifar-10-mobile-gate-emb/']
+            dataset_subroot = 'imagenet-20-client/'
+            ind = [str(x)+'.bmp' for x in range(600)]
+            quality = [10, 20, 30, 40, 50, 60, 70, 80, 90]
+            for i in range(len(quality)):
+                for j in range(len(ind)):
+                    image_path = dataset_root + dataset_subroot + ind[j]
+                    image = cv2.imread(image_path)
+                    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), quality[i]]
+                    result, img_code = cv2.imencode('.jpg', image, encode_param)
+                    img_code = img_code.tobytes()
+                    img_code = base64.b64encode(img_code)
+                    msg_length = pack('>Q', len(img_code))
                     self.s.sendall(msg_length)
-                    time.sleep(0.01)
-                    
-                    self.s.sendall(file)
-                    ttime1 = time.time()
-                    
-                    
-                    # wait for the done from the server
+                    self.s.sendall(img_code)
                     done = self.s.recv(1)
                     time.sleep(0.01)
-                    # print('send time: ', time.time() - ttime1)
+                time.sleep(3)
+
+            # dataset_subroot = ['last-imagenet-20-jpeg'+str(x) for x in range(10, 100, 10)]
+            # for dataset in dataset_subroot:
+            #     dataset = dataset_root + dataset
+            #     print('dataset: ', dataset)
+
+            #     file_names = [str(i) for i in range(100)]
+            #     files = []
+                # for i in range(len(file_names)):
+                #     # file_names[i] = Image.open(dataset + '/' + str(i)+'.jpg')
+                #     # file_names[i] = file_names[i].convert('RGB')
+                #     # # encode
+                #     # # file_names[i] = file_names[i].resize((224, 224))
+                #     # file_names[i] = file_names[i].tobytes()
+                #     # # encode
+                #     # file_names[i] = base64.b64encode(file_names[i])
+                #     # files.append(file_names[i])
+                #     file_names[i] = dataset + '/' + str(i)+'.jpg'
+
+                #     file_names[i] = cv2.imread(dataset + '/' + str(i)+'.jpg')
+                #     result, img_code = cv2.imencode('.jpg', file_names[i])
+                #     file_names[i] = img_code.tobytes()
+                #     files.append(file_names[i])
+                
+                # for i, file in enumerate(files):
+                #     # print(len(file))
+                #     # file = open(dataset + '/' + str(i), 'rb')
+                #     # file = file.read()
+                #     f = cv2.imread(dataset + '/' + str(i)+'.jpg')
+                #     result, f = cv2.imencode('.jpg', f[i])
+                #     f = f.tobytes()
+                #     msg_length = pack('>Q', len(f))
+                #     # time.sleep(0.01)
+                #     self.s.sendall(msg_length)
+                    
+                #     self.s.sendall(f)
+                #     # ttime1 = time.time()
+                    
+                    
+                #     # wait for the done from the server
+                #     done = self.s.recv(1)
+                #     # time.sleep(0.01)
+                #     # print('send time: ', time.time() - ttime1)
 
                 time.sleep(3)
         except Exception as e:

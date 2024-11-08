101b1f820c64134c994ed9481f841b4ba6c75c74
tony peng <tonyy_good@126.com>
2024-11-08 13:29:11 -0500

    home

---
+++ Models/encoder.py
@@ -0,0 +1,35 @@
+import torch.nn as nn
+import torch
+
+class Encoder(nn.Module):
+    def __init__(self, in_ch, num_of_layers = 2):
+        super(Encoder, self).__init__()
+        
+        # simple two layers CNN
+        # down sampling, this is for square image
+        # suppose reduce it from 56, 56 to 28, 28, or 16, 16
+        self.in_ch = in_ch
+        encoder_sequence = []
+        for i in range(num_of_layers):
+            encoder_sequence.append(nn.Conv2d(in_channels=self.in_ch, out_channels=2*self.in_ch, kernel_size=3, stride=2, padding=1))
+            encoder_sequence.append(nn.BatchNorm2d(2*self.in_ch))
+            encoder_sequence.append(nn.ReLU(True))
+            self.in_ch = 2*self.in_ch
+        self.encoder = nn.Sequential(*encoder_sequence)
+
+    def forward(self, x):
+        return self.encoder(x)
+    
+class Encoder_Gate(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Encoder_Gate, self).__init__()
+        
+        # reduce channel in_ch to out_ch
+        self.in_ch = in_ch
+        encoder_gate_sequence = []
+        while self.in_ch >= 1:
+            encoder_gate_sequence.append(nn.Conv2d(in_channels=self.in_ch, out_channels=self.in_ch//2, kernel_size=1))
+            encoder_gate_sequence.append(nn.BatchNorm2d(in_ch))
+            encoder_gate_sequence.append(nn.ReLU(True))
+            self.in_ch = self.in_ch//2
+        
--- train_model_imagenet_20_new_coder.py
+++ train_model_imagenet_20_new_coder.py
@@ -1,17 +1,32 @@
-from Models import mobilenetv2, resnet, last_classifier
+from Models import mobilenetv2, resnet, last_classifier, encoder, decoder
 import sys
 import torch
 
 model_type = sys.argv[1]
+num_of_layers = int(sys.argv[2]) # 2 for mobilenet, 1 for resnet
+
 if 'mobilenet' in model_type:
     client, server = mobilenetv2.mobilenetv2_splitter(num_classes=1000,
-                                                  weight_root='Weights/imagenet-new',
+                                                  weight_root='Weights/imagenet-new/',
                                                   device='cuda:0',partition=-1)
+    new_classifier = last_classifier.last_layer_classifier(1000, 20)
+    class_weight = torch.load('Weights/imagenet-new/lastlayer/mobilenet.pth')
+    new_classifier.load_state_dict(class_weight['model'])
 elif 'resnet' in model_type:
     client, server = mobilenetv2.mobilenetv2_splitter(num_classes=1000,
                                                   weight_root='Weights/imagenet-new/',
                                                   device='cuda:0',partition=-1)
+    new_classifier = last_classifier.last_layer_classifier(1000, 20)
+    class_weight = torch.load('Weights/imagenet-new/lastlayer/resnet.pth')
+    new_classifier.load_state_dict(class_weight['model'])
 new_classifier = last_classifier.last_layer_classifier(1000, 20)
+
+if 'mobilenet' in model_type:
+    enc = encoder.Encoder(in_ch=32, num_of_layers=num_of_layers)
+    dec = decoder.Decoder(in_ch=32*2**num_of_layers, num_of_layers=num_of_layers)
+elif 'resnet' in model_type:
+    enc = encoder.Encoder(in_ch=64, num_of_layers=num_of_layers)
+    dec = decoder.Decoder(in_ch=64*2**num_of_layers, num_of_layers=num_of_layers)
 
 import datetime
 model_time = datetime.datetime.now().strftime("%m%d%H%M%S")
@@ -29,25 +44,25 @@ import torch.optim as optim
 import torch.nn as nn
 
 device = torch.device('cuda:0')
-# client = client.to(device)
-# server = server.to(device)
-# client = client.train()
-# server = server.train()
 
 client = client.to(device)
 server = server.to(device)
 new_classifier = new_classifier.to(device)
+enc = enc.to(device)
+dec = dec.to(device)
 
 # freeze model parameters
 for param in client.parameters():
     param.requires_grad = False
 for param in server.parameters():
     param.requires_grad = False
+for param in new_classifier.parameters():
+    param.requires_grad = False
 
 criterion = nn.CrossEntropyLoss()
 # optimizer_client = optim.adam(client.parameters(), lr=0.001)
 # optimizer_server = optim.adam(server.parameters(), lr=0.001)
-optimizer = optim.Adam(new_classifier.parameters(), lr=0.001)
+optimizer = optim.Adam(list(enc.parameters()) + list(dec.parameters()), lr=0.001)
 
 from tqdm import tqdm
 from Utils import utils
@@ -59,8 +74,8 @@ import os
 epochs = 100
 max_val_acc = 0
 
-if not os.path.exists(f'Weights/training/{model_type}_new_classifier_{model_time}/'):
-    os.mkdir(f'Weights/training/{model_type}_new_classifier_{model_time}/')
+if not os.path.exists(f'Weights/training/{model_type}_coder_{model_time}/'):
+    os.mkdir(f'Weights/training/{model_type}_coder_{model_time}/')
 
 for epoch in range(epochs):
     train_loss = 0.0
@@ -68,13 +83,17 @@ for epoch in range(epochs):
     client.train()
     server.train()
     new_classifier.train()
+    enc.train()
+    dec.train()
 
     for i, (data, labels) in tqdm(enumerate(train)):
 
         data, labels = data.to(device), labels['label'].to(device)
 
-        pred = client(data)
-        pred = server(pred)
+        client_out = client(data)
+        enc_out = enc(client_out)
+        dec_out = dec(enc_out)
+        pred = server(dec_out)
         pred = new_classifier(pred)
 
         optimizer.zero_grad()
@@ -89,13 +108,18 @@ for epoch in range(epochs):
     client.eval()
     server.eval()
     new_classifier.eval()
+    enc.eval()
+    dec.eval()
 
     for i, (data, labels) in tqdm(enumerate(val)):
 
         data, labels = data.to(device), labels['label'].to(device)
-        pred = client(data)
-        pred = server(pred)
+        client_out = client(data)
+        enc_out = enc(client_out)
+        dec_out = dec(enc_out)
+        pred = server(dec_out)
         pred = new_classifier(pred)
+
         # get the number of 0 and 1
         pred = torch.softmax(pred, dim=1)
         pred = torch.argmax(pred, dim=1)
@@ -106,8 +130,15 @@ for epoch in range(epochs):
     print('val_acc: ', val_acc/len(val.dataset))
 
     torch.save({
-        'model': new_classifier.state_dict(),
+        'model': enc.state_dict(),
         'optimizer': optimizer.state_dict(),
         'epoch': epoch,
         'val_acc': val_acc
-    }, f'Weights/training/{model_type}_new_classifier_{model_time}/epoch-{epoch}-train-loss-{train_loss}-acc-{val_acc}.pth')
+    }, f'Weights/training/{model_type}_coder_{model_time}/encoder_epoch-{epoch}-train-loss-{train_loss}-acc-{val_acc}.pth')
+
+    torch.save({
+        'model': dec.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'epoch': epoch,
+        'val_acc': val_acc
+    }, f'Weights/training/{model_type}_coder_{model_time}/decoder_epoch-{epoch}-train-loss-{train_loss}-acc-{val_acc}.pth')

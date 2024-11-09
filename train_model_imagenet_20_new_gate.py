0a0e745fee65f030d84544f3dd8f8c0e9fa7db7c
tony peng <tonyy_good@126.com>
2024-11-09 00:56:09 -0500

    home

--- train_model_imagenet_20_new_gate.py
+++ train_model_imagenet_20_new_gate.py
@@ -1,4 +1,4 @@
-from Models import mobilenetv2, resnet, last_classifier, encoder, decoder, upsampler, downsampler
+from Models import mobilenetv2, resnet, last_classifier, encoder, decoder, upsampler, downsampler, gatedmodel
 import sys
 import torch
 
@@ -7,45 +7,48 @@ num_of_layers = int(sys.argv[2]) # 2 for mobilenet, 1 for resnet
 num_of_ch = int(sys.argv[3])
 
 if 'mobilenet' in model_type:
+    in_ch = 32
+    img_h, img_w = 112, 112
     client, server = mobilenetv2.mobilenetv2_splitter(num_classes=1000,
                                                   weight_root=None,
                                                   device='cuda:0',partition=-1)
     all_weights = f'Weights/imagenet-new/pretrained/mobilenet_{num_of_layers}.pth'
-    in_ch = 32
+    down = downsampler.Downsampler(in_ch=in_ch, num_of_layers=num_of_layers)
+    up = upsampler.Upsampler(in_ch=in_ch*(2**num_of_layers), num_of_layers=num_of_layers)
+    coder_weights = f'Weights/imagenet-new/pretrained/mobilenet_sampler_{num_of_layers}.pth'
+
 elif 'resnet' in model_type:
+    in_ch = 64
+    img_h, img_w = 56, 56
     client, server = resnet.resnet_splitter(num_classes=1000,
                                                   weight_root=None,
                                                   device='cuda:0', layers=50)
     all_weights = f'Weights/imagenet-new/pretrained/resnet_{num_of_layers}.pth'
-    in_ch = 64
+    down = downsampler.Downsampler(in_ch=in_ch, num_of_layers=num_of_layers)
+    up = upsampler.Upsampler(in_ch=in_ch*(2**num_of_layers), num_of_layers=num_of_layers)
+    coder_weights = f'Weights/imagenet-new/pretrained/resnet_sampler_{num_of_layers}.pth'
+
 classifier = last_classifier.last_layer_classifier(1000, 20)
-if 'mobilenet' in model_type:
-    down = downsampler.Downsampler(in_ch=32, num_of_layers=num_of_layers)
-    up = upsampler.Upsampler(in_ch=32*2**num_of_layers, num_of_layers=num_of_layers)
-elif 'resnet' in model_type:
-    down = downsampler.Downsampler(in_ch=64, num_of_layers=num_of_layers)
-    up = upsampler.Upsampler(in_ch=64*2**num_of_layers, num_of_layers=num_of_layers)
-    
+enc = encoder.Encoder(in_ch=in_ch*(2**num_of_layers), out_ch=num_of_ch)
+dec = decoder.Decoder(in_ch=num_of_ch, out_ch=in_ch*(2**num_of_layers))
 
 checkpoint = torch.load(all_weights)
-print(checkpoint.keys())
 client.load_state_dict(checkpoint['client'])
 server.load_state_dict(checkpoint['server'])
 up.load_state_dict(checkpoint['upsampler'])
 down.load_state_dict(checkpoint['downsampler'])
 classifier.load_state_dict(checkpoint['new_classifier'])
 
+coder_checkpoint = torch.load(coder_weights)
+enc.load_state_dict(coder_checkpoint['encoder'])
+dec.load_state_dict(coder_checkpoint['decoder'])
+
 import datetime
 model_time = datetime.datetime.now().strftime("%m%d%H%M%S")
 
-# last_channel = server.last_channel
-
-# from Models import last_classifier
-# replace_layer = last_classifier.Last_classifier(last_channel, 20)
-
 from Dataloaders import dataloader_image_20_new
 
-train, test, val = dataloader_image_20_new.Dataloader_imagenet_20_integrated(train_batch=64, test_batch=32)
+train, test, val = dataloader_image_20_new.Dataloader_imagenet_20_integrated(train_batch=128, test_batch=64)
 
 import torch.optim as optim
 import torch.nn as nn
@@ -57,6 +60,8 @@ server = server.to(device)
 classifier = classifier.to(device)
 down = down.to(device)
 up = up.to(device)
+enc = enc.to(device)
+dec = dec.to(device)
 
 # freeze model parameters
 for param in client.parameters():
@@ -69,16 +74,16 @@ for param in up.parameters():
     param.requires_grad = False
 for param in classifier.parameters():
     param.requires_grad = False
+for param in enc.parameters():
+    param.requires_grad = False
+for param in dec.parameters():
+    param.requires_grad = False
 
-enc = encoder.Encoder(in_ch=in_ch*(2**num_of_layers), out_ch=num_of_ch)
-dec = decoder.Decoder(in_ch=num_of_ch, out_ch=in_ch*(2**num_of_layers))
+gate = gatedmodel.ExitGate(in_planes=num_of_ch, height=img_h, width=img_w)
 
-enc = enc.to(device)
-dec = dec.to(device)
+criterion = nn.MSELoss()
 
-criterion = nn.CrossEntropyLoss()
-
-optimizer = optim.Adam(list(enc.parameters()) + list(dec.parameters()), lr=0.001)
+optimizer = optim.Adam(gate.parameters(), lr=0.001)
 
 from tqdm import tqdm
 from Utils import utils
@@ -90,20 +95,21 @@ import os
 epochs = 30
 max_val_acc = 0
 
-if not os.path.exists(f'Weights/training/{model_type}_coder_{num_of_layers}_{num_of_ch}_{model_time}/'):
-    os.mkdir(f'Weights/training/{model_type}_coder_{num_of_layers}_{num_of_ch}_{model_time}/')
-print('saving to: ', f'Weights/training/{model_type}_coder_{num_of_layers}_{num_of_ch}_{model_time}/')
+if not os.path.exists(f'Weights/training/{model_type}_gate_{num_of_layers}_{model_time}/'):
+    os.mkdir(f'Weights/training/{model_type}_gate_{num_of_layers}_{model_time}/') 
+print('saving to: ', f'Weights/training/{model_type}_gate_{num_of_layers}_{model_time}/')
+
+client.eval()
+server.eval()
+classifier.eval()
+down.eval()
+up.eval()
+enc.eval()
+dec.eval()
 
 for epoch in range(epochs):
     train_loss = 0.0
-
-    client.train()
-    server.train()
-    classifier.train()
-    down.train()
-    up.train()
-    enc.train()
-    dec.train()
+    gate.train()
 
     for i, (data, labels) in tqdm(enumerate(train)):
 
@@ -112,13 +118,20 @@ for epoch in range(epochs):
         output = client(data)
         output = down(output)
         output = enc(output)
+
+        gate_score = gate(output)
+
         output = dec(output)
         output = up(output)
         pred = server(output)
         pred = classifier(pred)
 
+        # the confidence of the correct label
+        pred = torch.softmax(pred, dim=1)
+        conf = pred[torch.arange(pred.size(0)), labels] # cool        
+
         optimizer.zero_grad()
-        loss = criterion(pred, labels)
+        loss = criterion(gate_score, pred)
         train_loss += loss.item()
         loss.backward()
         optimizer.step()
@@ -126,14 +139,7 @@ for epoch in range(epochs):
     print('train loss: ', train_loss)
 
     val_acc = 0
-
-    client.eval()
-    server.eval()
-    classifier.eval()
-    down.eval()
-    up.eval()
-    enc.eval()
-    dec.eval()
+    gate.eval()
 
     for i, (data, labels) in tqdm(enumerate(test)):
 
@@ -142,6 +148,9 @@ for epoch in range(epochs):
         output = client(data)
         output = down(output)
         output = enc(output)
+
+        gate_score = gate(output)
+
         output = dec(output)
         output = up(output)
         output = server(output)
@@ -150,8 +159,12 @@ for epoch in range(epochs):
         # get the number of 0 and 1
         pred = torch.softmax(pred, dim=1)
         pred = torch.argmax(pred, dim=1)
-        accuracy = torch.eq(pred, labels).float()
 
+        gate_threshold = 0.5
+        gate_eval = torch.where(gate_score > gate_threshold, torch.tensor([1.]).to(device), torch.tensor([0.]).to(device))
+        pred_eval = torch.where(pred == labels, torch.tensor([1.]).to(device), torch.tensor([0.]).to(device))
+
+        accuracy = torch.eq(gate_eval, pred_eval).float()
         # print the rate of gate exit
         val_acc += accuracy.sum().item()
     val_acc = val_acc/len(test.dataset)
@@ -160,10 +173,9 @@ for epoch in range(epochs):
     if val_acc > max_val_acc:
         max_val_acc = val_acc
         torch.save({
-            'encoder': enc.state_dict(),
-            'decoder': dec.state_dict(), 
+            'gate': gate.state_dict(),
             'optimizer': optimizer.state_dict(),
             'epoch': epoch,
             'val_acc': val_acc
-        }, f'Weights/training/{model_type}_coder_{num_of_layers}_{num_of_ch}_{model_time}/coder_best_model.pth')
+        }, f'Weights/training/{model_type}_gate_{num_of_layers}_{num_of_ch}_{model_time}/gate_best_model.pth')
         print('model saved' + ' train loss ', train_loss, ' val acc ', val_acc)
